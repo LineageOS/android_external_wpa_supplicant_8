24e938e1606a5886dd26f1ee4faf34f9253fca20
vamsi krishna <vamsin@qti.qualcomm.com>
2016-09-09 17:35:50 +0530

    MBO: Add support to send ANQP request to get cellular preference
    This extends ANQP_GET command to support querying MBO cellular
    preference also. The cellular preference can be requested along with
    neigbor report by appending mbo:1 to the command arguments.
    
    For example:
    ANQP_GET <bssid> 272,mbo:1
    
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>
    Git-commit: 8f4791749302bb3a89b2baef19fc5e6075ec3845
    Git-repo: git://w1.fi/srv/git/hostap.git
    Change-Id: Ic242de6aa946ced65dd2c954af29b763a8af3757
    CRs-Fixed: 1070010

--- wpa_supplicant/ctrl_iface.c
+++ wpa_supplicant/ctrl_iface.c
@@ -6388,6 +6388,7 @@ static int get_anqp(struct wpa_supplicant *wpa_s, char *dst)
 	u16 id[MAX_ANQP_INFO_ID];
 	size_t num_id = 0;
 	u32 subtypes = 0;
+	int get_cell_pref = 0;
 
 	used = hwaddr_aton2(dst, dst_addr);
 	if (used < 0)
@@ -6405,6 +6406,15 @@ static int get_anqp(struct wpa_supplicant *wpa_s, char *dst)
 #else /* CONFIG_HS20 */
 			return -1;
 #endif /* CONFIG_HS20 */
+		} else if (os_strncmp(pos, "mbo:", 4) == 0) {
+#ifdef CONFIG_MBO
+			int num = atoi(pos + 4);
+			if (num != MBO_ANQP_SUBTYPE_CELL_CONN_PREF)
+				return -1;
+			get_cell_pref = 1;
+#else /* CONFIG_MBO */
+			return -1;
+#endif /* CONFIG_MBO */
 		} else {
 			id[num_id] = atoi(pos);
 			if (id[num_id])
@@ -6419,7 +6429,8 @@ static int get_anqp(struct wpa_supplicant *wpa_s, char *dst)
 	if (num_id == 0)
 		return -1;
 
-	return anqp_send_req(wpa_s, dst_addr, id, num_id, subtypes);
+	return anqp_send_req(wpa_s, dst_addr, id, num_id, subtypes,
+			     get_cell_pref);
 }
 
 
